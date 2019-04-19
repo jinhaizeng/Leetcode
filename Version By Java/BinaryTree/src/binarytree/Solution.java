9654ded0b2223420d4fe98d5a493e4364756df94
jinhaiZeng <1270417137@qq.com>
2019-04-19 10:21:22 +0800

    更新图床

--- Version By Java/BinaryTree/src/binarytree/Solution.java
+++ Version By Java/BinaryTree/src/binarytree/Solution.java
@@ -1,7 +1,6 @@
 package binarytree;
 
 import java.util.List;
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -11,17 +10,15 @@ import java.util.List;
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
-        }
-    }
+
+public class Solution {
+
     public List<Integer> preorderTraversal(TreeNode root) {
+        preorderTraversal(root.left);
+//        if(root)
+//            return root.val;
+        preorderTraversal(root.right);
 
     }
 }
