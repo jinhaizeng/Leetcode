7b9c7c6729d0f38b1ebd75fb85fe54377f779501
Ryan <1270417137@qq.com>
2020-12-07 13:39:55 +0800

    午休随手AC了题，晚上看下解答的其他解法

---
+++ top100/No48_userReadAnalyse.cpp
@@ -0,0 +1,18 @@
+    class Solution {
+    public:
+        void rotate(vector<vector<int>>& matrix) {
+            int length = matrix[0].size();
+            //矩阵转置
+            for (int i = 0; i < length; ++i) {
+                for (int j=i; j < length; ++j) {
+                    swap(matrix[j][i], matrix[i][j]);
+                }
+            }
+            //矩阵列翻转
+            for (int i=0; i < length; ++i) {
+                for (int j=i; j <= length/2; ++j) {
+                    swap(matrix[i][j], matrix[i][length - 1 -j]);
+                }
+            }
+        }
+    };
