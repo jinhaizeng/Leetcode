9cb8f97c8012b8016ec7d0702ac3c856f9f86dc8
Ryan <1270417137@qq.com>
2020-12-29 23:07:02 +0800

    随手AC，题目96一刷没有想到思路，需要二刷

---
+++ top100/No_96_user.java
@@ -0,0 +1,21 @@
+//错误写法，直接pass
+class Solution {
+    int result = 0;
+    public int numTrees(int n) {
+        int[] visit = new int[n];
+    }
+    public void dfs (int index, int[] visit, int n, int count) {
+        if (visit[index]) {
+            return;
+        }
+        if (index < )
+        if (count == n) {
+            result++;
+            return;
+        }
+
+        for(int i = 0; i < n; ++i) {
+            if ()
+        }
+    }
+}
